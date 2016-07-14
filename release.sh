055c464ceec473c14f8539780bac715c677dc338
makotot <makoto.ttn@gmail.com>
2016-07-15 00:34:52 +0900

    * sh

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@
 
 rm -rf release && mkdir release;
 npm run build;
-cp ./{scrollspy.js,README.md,package.json,gulpfile.js,.eslintrc} ./release/
+cp scrollspy.js README.md package.json gulpfile.js .eslintrc ./release
 
-rm -rf *.tar.gz
-rm -rf *.zip
+rm -rf react-scrollspy.tar.gz
+rm -rf react-scrollspy.zip
 tar -zcvf react-scrollspy.tar.gz ./release
 zip -r react-scrollspy.zip ./release
